da83e9daf36a84af4eaa7d66d7ea9692df4627af
tinofay <tfzungunde@outlook.com>
2024-10-23 19:05:47 +0200

    add reducers

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -255,7 +255,7 @@ const handlePlantsClick = (e) => {
         dispatch(addItem(product));
         setAddedToCart((prevState) => ({
             ...prevState,
-            [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+            [product.name]: true,
         }));
     };
 
