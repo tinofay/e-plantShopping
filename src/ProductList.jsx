dac3ef67c83f332224f4652e8bb6239ec4751188
tinofay <tfzungunde@outlook.com>
2024-12-18 13:22:22 -0500

    add cost and description to cards

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -289,6 +289,8 @@ const handlePlantsClick = (e) => {
                             <div className="product-card" key={plantIndex}>
                                 <img className="product-image" src={plant.image} alt={plant.name} />
                                 <div className="product-title">{plant.name}</div>
+                                <div className="product-description">{plant.description}</div>
+                                <div className="product-cost">{plant.cost}</div>
                                 <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                             </div>
                         ))}
